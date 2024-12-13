bf9e93448324890c783606e77254806f8a0c2a30
FinnAppel <113428508+FinnAppel@users.noreply.github.com>
2024-12-13 13:41:42 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,31 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Detect operating system
+OS="$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '\"')"
+
+if [[ "$OS" == "Ubuntu"* ]]; then
+    WEBSERVER_USER="www-data"
+    WEBSERVER_GROUP="www-data"
+    echo "Ubuntu detected. Using $WEBSERVER_USER:$WEBSERVER_GROUP for ownership."
+elif [[ "$OS" == "CentOS"* ]]; then
+    if systemctl is-active --quiet nginx; then
+        WEBSERVER_USER="nginx"
+        WEBSERVER_GROUP="nginx"
+        echo "CentOS with Nginx detected. Using $WEBSERVER_USER:$WEBSERVER_GROUP for ownership."
+    elif systemctl is-active --quiet httpd; then
+        WEBSERVER_USER="apache"
+        WEBSERVER_GROUP="apache"
+        echo "CentOS with Apache detected. Using $WEBSERVER_USER:$WEBSERVER_GROUP for ownership."
+    else
+        echo "CentOS detected but no supported web server (nginx or apache) is active. Exiting."
+        exit 1
+    fi
+else
+    echo "Unsupported operating system: $OS. Exiting."
+    exit 1
+fi
+
 # Define the Pterodactyl directory
 PTERODACTYL_DIR="/var/www/pterodactyl"
 
@@ -28,8 +53,8 @@ php artisan config:clear
 # Run database migrations and seeders
 php artisan migrate --seed --force
 
-# Set ownership of files to the web server user
-ochown -R www-data:www-data "$PTERODACTYL_DIR/*"
+# Set ownership of files to the web server user and group
+chown -R "$WEBSERVER_USER":"$WEBSERVER_GROUP" "$PTERODACTYL_DIR/*"
 
 # Restart the queue workers
 php artisan queue:restart
